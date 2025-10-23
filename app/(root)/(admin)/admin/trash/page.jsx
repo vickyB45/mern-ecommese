3ac518d074e57740fd6bbea0bedb83943a82a443
vimal singh <vimal.s@optystack.com>
2025-10-23 11:04:02 +0530

    add trash page in admin panel for orders

--- app/(root)/(admin)/admin/trash/page.jsx
+++ app/(root)/(admin)/admin/trash/page.jsx
@@ -4,7 +4,7 @@ import BreadCrumb from "@/components/Aplication/admin/BreadCrumb";
 import DataTableWraper from "@/components/Aplication/admin/DataTableWraper";
 import DeleteAction from "@/components/Aplication/admin/DeleteAction";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { DT_CATEGORY_COLUMN, DT_COSTOMERS_COLUMN, DT_COUPON_COLUMN, DT_PRODUCT_COLUMN, DT_PRODUCT_VARIANT_COLUMN, DT_REVIEW_COLUMN } from "@/lib/Column";
+import { DT_CATEGORY_COLUMN, DT_COSTOMERS_COLUMN, DT_COUPON_COLUMN, DT_ORDER_COLUMN, DT_PRODUCT_COLUMN, DT_PRODUCT_VARIANT_COLUMN, DT_REVIEW_COLUMN } from "@/lib/Column";
 import { columnConfig } from "@/lib/helperFunctions";
 import {
   ADMIN_DASHBOARD,
@@ -60,6 +60,13 @@ const TRASH_CONFIG = {
     fetchUrl: "/api/review",
     exportUrl: "/api/review/export",
     deleteUrl: "/api/review/delete",
+  },
+  orders: {
+    title: " Orders Trash",
+    columns: DT_ORDER_COLUMN,
+    fetchUrl: "/api/orders",
+    exportUrl: "/api/orders/export",
+    deleteUrl: "/api/orders/delete",
   },
 };
 
